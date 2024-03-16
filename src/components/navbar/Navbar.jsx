5611ff0bc1b9a8c1f19bb12f57a33a519443294c
aidoha <ibrayevaiqyn@gmail.com>
2024-03-17 01:02:01 +0500

    feat:add navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from '../../../public/assets/givemeLogo.svg';
+import Image from 'next/image';
+
+const Navbar = () => {
+  return (
+    <div className="text-white">
+      <nav className="bg-black">
+        <div className="container  h-20 flex items-center justify-around">
+          <div>
+            <Image src={logo}></Image>
+          </div>
+          <div className="flex gap-8">
+            <span>Main</span>
+            <span>Give</span>
+            <span>Receive</span>
+            <span>Calculator</span>
+            <span>About us</span>
+            <div>
+              <button className="bg-pink-400  w-24 rounded-sm">Sign in</button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
