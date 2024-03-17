74485ed441a476bc576a31bfea48dbce08636349
aidoha <ibrayevaiqyn@gmail.com>
2024-03-17 16:14:19 +0500

    feat:add main part

--- src/components/uploadPage/Card/Card.jsx
+++ src/components/uploadPage/Card/Card.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
+import Image from 'next/image';
 import clothes from '../../../../public/assets/clothes.svg';
 import toys from '../../../../public/assets/toys.svg';
 import shoes from '../../../../public/assets/shoes.svg';
+import addFile from '../../../../public/assets/addFile.svg';
+import deleteLogo from '../../../../public/assets/delete.svg';
 import { CARD_TEXT } from './string';
 import SubCard from './SubCard';
+
 const data = [
   {
     text: 'CLOTHES',
@@ -18,7 +23,15 @@ const data = [
     image: shoes,
   },
 ];
+
 const Card = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
   return (
     <div>
       <div className="mt-24 ml-36">
@@ -30,9 +43,31 @@ const Card = () => {
           <span>{CARD_TEXT.SEPARTION}</span>
         </div>
         <div className="flex gap-6 mt-5">
-          {data.map((item, index) => {
-            return <SubCard key={index} text={item.text} image={item.image} />;
-          })}
+          {data.map((item, index) => (
+            <SubCard key={index} text={item.text} image={item.image} />
+          ))}
+        </div>
+        <div className="mt-24">
+          <div className="text-3xl font-bold">
+            <span>{CARD_TEXT.UPLOAD}</span>
+          </div>
+          <div className="mt-6">
+            <span>{CARD_TEXT.SUB_UPLOAD}</span>
+          </div>
+          <div className="bg-white w-4/5 rounded-lg border h-[220px] mt-4 border-gray-300 flex items-center justify-center space-x-2">
+            <label htmlFor="fileInput" className="cursor-pointer">
+              <Image src={addFile} width={24} height={24} alt="Add File" />
+            </label>
+            <input id="fileInput" type="file" className="hidden" onChange={handleFileChange} />
+          </div>
+          <div className="mt-4">
+            {selectedFile && (
+              <div className="inline-flex items-center pr-4  bg-white">
+                <span className="text-lg">{selectedFile.name}</span>
+                <Image src={deleteLogo} className="ml-3" />
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/components/uploadPage/Card/string.js
+++ src/components/uploadPage/Card/string.js
@@ -1,7 +1,8 @@
 export const CARD_TEXT = {
   OFFER: 'What do you want to offer ?',
   DONATION:
-  'Select the type of item you want to offer: for sale or as a donation. If you have several things',
+    'Select the type of item you want to offer: for sale or as a donation. If you have several things',
   SEPARTION: 'of different types, you should place them separately.',
+  UPLOAD: 'Upload a few photos of your items:',
+  SUB_UPLOAD: 'Upload one general photo containing one or more unnecessary clothes.',
 };
-
