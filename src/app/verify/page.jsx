fed2213f238315010a7183954afb85688d7c8fbd
aidoha <ibrayevaiqyn@gmail.com>
2024-04-01 20:13:23 +0500

    fix:suspense error

--- src/app/verify/page.jsx
+++ src/app/verify/page.jsx
@@ -1,31 +1,37 @@
 'use client';
-import { redirect } from 'next/navigation';
-import axios from 'axios';
-import { useSearchParams } from 'next/navigation';
 import { Suspense, useEffect } from 'react';
+import { useSearchParams, redirect } from 'next/navigation';
+import axios from 'axios';
 export default function Page() {
-  const tokenParams = useSearchParams();
-  const token = new URLSearchParams(tokenParams.toString()).get('token');
-  useEffect(() => {
-    async function verify() {
-      try {
-        const res = await axios.post(`${process.env.baseURL}/user/activate_account/`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        if (res.status === 200) {
-          redirect('/login');
+  const PageContent = () => {
+    const tokenParams = useSearchParams();
+    const token = new URLSearchParams(tokenParams.toString()).get('token');
+    useEffect(() => {
+      async function verify() {
+        try {
+          const res = await axios.get(`${process.env.baseURL}/user/activate_account/`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          if (res.status === 200) {
+            redirect('/login');
+          }
+        } catch (e) {
+          console.error(e);
         }
-      } catch (e) {
-        console.error(e);
       }
-    }
-    verify();
-  }, []);
+      if (token) {
+        verify();
+      }
+    }, [token]);
+
+    return null;
+  };
+
   return (
-    <Suspense>
-      <div></div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <PageContent />
     </Suspense>
   );
 }
