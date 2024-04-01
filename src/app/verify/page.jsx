9e05c93ff03dc3aa42c780014ca6caa15ba0c655
aidoha <ibrayevaiqyn@gmail.com>
2024-04-01 16:14:37 +0500

    feat:add verffy gmail

---
+++ src/app/verify/page.jsx
@@ -0,0 +1,26 @@
+'use client';
+import { redirect } from 'next/navigation';
+import axios from 'axios';
+import { useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+export default function Page() {
+  const tokenParams = useSearchParams();
+  const token = new URLSearchParams(tokenParams.toString()).get('token');
+  useEffect(() => {
+    async function verify() {
+      try {
+        const res = await axios.post(`${process.env.baseURL}/user/activate_account/`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        if (res.status === 200) {
+          redirect('/login');
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    verify();
+  }, []);
+}
