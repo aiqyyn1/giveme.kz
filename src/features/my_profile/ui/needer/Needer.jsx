3605c8a2914383d20652f975793ea208d67a5173
aidoha <ibrayevaiqyn@gmail.com>
2024-04-13 21:42:40 +0500

    fix/profile

---
+++ src/features/my_profile/ui/needer/Needer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Needer = () => {
+  return (
+    <div>
+      <span>If you needer</span>
+      <div>
+        <span>To check your result, we ask you to provide the following documents:</span>
+        <span>Electronic ID: Please upload a scanned image of your official proof of identity document, such as a passport or your driver's license.</span>
+        <span>Benefit Documents: If you have any documents confirming your status as a benefit recipient, please provide them for further verification.</span>
+        <span>Photos: We also ask that you provide several photographs of your residence, including photographs of your documents and photographs of yourself.</span>
+      </div>
+    </div>
+  );
+};
+
+export default Needer;
