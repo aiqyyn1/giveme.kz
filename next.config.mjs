30f0c37ef79ff3fe283b6e3c1112c486ab041abe
aidoha <ibrayevaiqyn@gmail.com>
2024-10-27 11:24:08 +0500

    fix:smt

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
+
   images: {
     domains: ['s3.eu-central-1.amazonaws.com'],
   },
--- src/app/main/layout.jsx
+++ src/app/main/layout.jsx
@@ -3,7 +3,7 @@ import Navbar from '../../widgets/navbar/ui';
 import Footer from '../../widgets/footer/Footer';
 export default function layout({ children }) {
   return (
-    <div className='font-DM'>
+    <div className="font-DM">
       <Navbar />
       {children}
       <Footer />
